ce98323a5bbd54f3c3fa2e5ba819228428736699
Michael Crosby <crosbymichael@gmail.com>
2014-09-24 00:01:05 -0700

    Update for rethinkdb

--- monitor.py
+++ monitor.py
@@ -1,13 +1,15 @@
 from psutil import cpu_times, cpu_percent, virtual_memory, swap_memory, disk_usage
 from time import sleep
 from os import getenv
+import socket
 import rethinkdb as r
 
 duration = int(getenv('DURATION', 60))
 
-def collect_data():
+def collect_data(hostname):
     return {
             'timestamp': r.now(),
+            'host': hostname,
             'cpu': cpu_percent(),
             'memory': virtual_memory().percent,
             'swap': swap_memory().percent,
@@ -15,20 +17,18 @@ def collect_data():
     }
 
 def monitor(conn, table):
+    hostname = socket.gethostname()
     while True:
-        data = collect_data()
+        data = collect_data(hostname)
         table.insert(data).run(conn)
         sleep(duration)
     conn.close()
 
 def main():
-    url = getenv('RETHINK_ADDR')
+    url = getenv('RETHINKDB_ADDR')
     if not url:
-        raise Exception("RETHINK_ADDR not found in environment")
-    db_name = getenv('RETHINK_NAME')
-    if not db_name:
-        raise Exception("RETHINK_NAME not found in environment")
-    conn = r.connect(host=url, db=db_name)
-    monitor(conn, r.db(db_name).table('metrics_system'))
+        raise Exception("RETHINKDB_ADDR not found in environment")
+    conn = r.connect(host=url, db='metrics')
+    monitor(conn, r.db(db_name).table('hosts'))
 
 if __name__=='__main__': main()
